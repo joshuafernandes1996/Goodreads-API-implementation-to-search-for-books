a32ba4ce529854881e1b56af7a19602b11b4ae5e
Joshua <jf0273@gmail.com>
2019-02-11 12:09:27 +0530

    UI Fix, added Interfaces, utility and code optimzation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import { tap } from "rxjs/operators";
 if (environment.production) {
   enableProdMode();
   
